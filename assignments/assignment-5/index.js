b68822af7b8715d22806f42ba32321c1fc44fbc5
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-06-04 15:17:28 -0400

    Fix the quotes

--- assignments/assignment-5/index.js
+++ assignments/assignment-5/index.js
@@ -1,5 +1,5 @@
 var name = "John";
-var age = 30;
+var age = "30";
 var city = "New York";
 var profession = "software engenieer";
 var hobby1 = "reading books";
@@ -10,5 +10,5 @@ var dreamTravelDestination = "Tokyo";
 var favoriteSeason = "autumn";
 
 console.log(
-    `Hello, my  name is ${name} and I am ${age} years old. I live in ${city}, where I work as a ${profession}. In my free time I enjot ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${blue}. my dream is  to one day visit ${dreamTravelDestination}. My favorite season of the years is ${favoriteSeason} because of the beautiful changing colors of the leaves.`
+    "Hello, my  name is ${name} and I am ${age} years old. I live in ${city}, where I work as a ${profession}. In my free time I enjot ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${blue}. my dream is  to one day visit ${dreamTravelDestination}. My favorite season of the years is ${favoriteSeason} because of the beautiful changing colors of the leaves."
 );
