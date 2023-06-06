33f904f33fe8a05260616a45ee809863647f64f8
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-06-06 12:34:47 -0400

    Finish assignment-6

--- assignments/assignment-5/index.js
+++ assignments/assignment-5/index.js
@@ -1,14 +1,26 @@
 var name = "John";
+console.log(name);
 var age = "30";
+console.log(age);
 var city = "New York";
+console.log(city);
 var profession = "software engenieer";
+console.log(profession);
 var hobby1 = "reading books";
+console.log(hobby1);
 var hobby2 = "playing the guitar";
+console.log(hobby2);
 var favoriteFood = "pizza";
+console.log(favoriteFood);
 var favoriteColor = "blue";
+console.log(favoriteColor);
 var dreamTravelDestination = "Tokyo";
+console.log(dreamTravelDestination);
 var favoriteSeason = "autumn";
+console.log(favoriteSeason);
 
-console.log(
-    "Hello, my  name is ${name} and I am ${age} years old. I live in ${city}, where I work as a ${profession}. In my free time I enjot ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${blue}. my dream is  to one day visit ${dreamTravelDestination}. My favorite season of the years is ${favoriteSeason} because of the beautiful changing colors of the leaves."
-);
+// console.log(
+//     "Hello, my  name is ${name} and I am ${age} years old. I live in ${city}, where I work as a ${profession}. In my free time I enjot ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${blue}. my dream is  to one day visit ${dreamTravelDestination}. My favorite season of the years is ${favoriteSeason} because of the beautiful changing colors of the leaves."
+// );
+var result = `Hello, my  name is ${name} and I am ${age} years old. I live in ${city}, where I work as a ${profession}. In my free time I enjot ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${favoriteColor}. my dream is  to one day visit ${dreamTravelDestination}. My favorite season of the years is ${favoriteSeason} because of the beautiful changing colors of the leaves.`;
+console.log(result);
